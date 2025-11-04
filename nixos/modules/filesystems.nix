18fbdec64dbd81c75b326b7509b7efe49b8a4580
uthoughtuatehuh <137680488+uthoughtuatehuh@users.noreply.github.com>
2025-11-04 15:47:03 +0000

    Delete nixos directory

--- nixos/modules/filesystems.nix
+++
@@ -1,8 +0,0 @@
-{ config, pkgs, ... }:
-{
-  fileSystems."/mnt/d" = {
-    device = "/dev/disk/by-uuid/dae27629-fcd7-4895-8b94-bd03667d38bd";
-    fsType = "ext4";
-    options = [ "defaults" "noatime" ];
-  };
-}
