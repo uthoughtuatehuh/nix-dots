75ebd3c2a3ea56bfae4a4bcdcc6c5884c0a511d6
uthoughtuatehuh <quanvepluxary@proton.me>
2025-12-23 10:04:34 +0300

    Unnecessary modules removed

--- modules/home/home.nix
+++ modules/home/home.nix
@@ -9,9 +9,7 @@ let
   profileImports = map (profile: ./profiles + "/${profile}/default.nix") enabledProfiles;
 in {
   imports = [
-    # ./packages.nix
     ./default.nix
-    # ./home/desktop.nix
   ] ++ profileImports;
 
   home.username = userConfig.username;
